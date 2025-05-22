8b52f14684600d5222f86a7aaae1c9a914893fc0
LouisYRYJ <louis.yousif@yahoo.de>
2025-05-22 10:56:09 +0000

    EK-FAC running

---
+++ quelle/approx_unrolling/EK_FAC.py
@@ -0,0 +1,200 @@
+import argparse
+import logging
+import os
+from typing import Dict
+
+import torch
+from examples.wikitext.pipeline import get_wikitext_dataset
+from kronfluence.analyzer import Analyzer, prepare_model
+from kronfluence.arguments import FactorArguments
+from kronfluence.utils.common.factor_arguments import all_low_precision_factor_arguments
+from kronfluence.utils.dataset import DataLoaderKwargs
+from language_task import LanguageModelingTask
+from transformers import GPTNeoXForCausalLM, default_data_collator
+
+BATCH_TYPE = Dict[str, torch.Tensor]
+
+
+def parse_args():
+    parser = argparse.ArgumentParser(
+        description="Influence analysis on WikiText dataset."
+    )
+
+    parser.add_argument(
+        "--checkpoint_dir",
+        type=str,
+        default="./checkpoints",
+        help="A path that is storing the final checkpoint of the model.",
+    )
+
+    parser.add_argument(
+        "--factor_strategy",
+        type=str,
+        default="ekfac",
+        help="Strategy to compute influence factors.",
+    )
+    parser.add_argument(
+        "--query_gradient_rank",
+        type=int,
+        default=-1,
+        help="Rank for the low-rank query gradient approximation.",
+    )
+    parser.add_argument(
+        "--use_half_precision",
+        action="store_true",
+        default=False,
+        help="Whether to use half precision for computing factors and scores.",
+    )
+    parser.add_argument(
+        "--use_compile",
+        action="store_true",
+        default=False,
+        help="Whether to use torch compile for computing factors and scores.",
+    )
+    parser.add_argument(
+        "--query_batch_size",
+        type=int,
+        default=8,
+        help="Batch size for computing query gradients.",
+    )
+    parser.add_argument(
+        "--train_batch_size",
+        type=int,
+        default=8,
+        help="Batch size for computing query gradients.",
+    )
+    parser.add_argument(
+        "--per_device_batch_size",
+        type=int,
+        default=None,
+        help="Batch size for computing matrices.",
+    )
+    parser.add_argument(
+        "--compute_per_token_scores",
+        action="store_true",
+        default=False,
+        help="Boolean flag to compute per token scores.",
+    )
+    parser.add_argument(
+        "--profile",
+        action="store_true",
+        default=False,
+        help="Boolean flag to profile computations.",
+    )
+
+    args = parser.parse_args()
+
+    if args.checkpoint_dir is not None:
+        os.makedirs(args.checkpoint_dir, exist_ok=True)
+
+    return args
+
+
+def main():
+    args = parse_args()
+    logging.basicConfig(level=logging.INFO)
+    # args = SimpleNamespace(
+    #     query_batch_size=32,
+    #     train_batch_size=64,
+    #     checkpoint_dir="/home/louis/kronfluence/examples/wikitext/checkpoints",
+    #     factor_strategy="ekfac",
+    #     use_half_precision=True,
+    #     use_compile=False,
+    #     compute_per_token_scores=False,
+    #     query_gradient_rank=-1,
+    #     profile=False,
+    #     per_device_batch_size=32,
+    # )
+    # Prepare the dataset.
+    train_dataset = get_wikitext_dataset(
+        split="eval_train",
+    )
+    # eval_dataset = get_wikitext_dataset(
+    #     split="valid",
+    # )
+
+    # Prepare the trained model.
+    # model = construct_gpt2()
+
+    model_str = "EleutherAI/pythia-14m"
+    step = 143000
+    model = GPTNeoXForCausalLM.from_pretrained(
+        model_str,
+        revision=f"step{step}",
+        device_map="cuda:0",
+    )
+
+    # checkpoint_path = os.path.join(args.checkpoint_dir, "model.pth")
+    # if not os.path.isfile(checkpoint_path):
+    #     raise ValueError(f"No checkpoint found at {checkpoint_path}.")
+    # model.load_state_dict(torch.load(checkpoint_path))
+
+    # Define task and prepare model.
+    task = LanguageModelingTask(module_keys=[m[0] for m in model.named_modules()])
+    model = prepare_model(model, task)
+
+    if args.use_compile:
+        model = torch.compile(model)
+
+    analyzer = Analyzer(
+        analysis_name="wikitext",
+        model=model,  # type: ignore (because)
+        task=task,
+        profile=args.profile,
+    )
+    # Configure parameters for DataLoader.
+    dataloader_kwargs = DataLoaderKwargs(collate_fn=default_data_collator)
+    analyzer.set_dataloader_kwargs(dataloader_kwargs)
+
+    # Compute influence factors.
+    factors_name = args.factor_strategy
+    factor_args = FactorArguments(strategy=args.factor_strategy)
+    if args.use_half_precision:
+        factor_args = all_low_precision_factor_arguments(
+            strategy=args.factor_strategy, dtype=torch.bfloat16
+        )
+        factors_name += "_half"
+    if args.use_compile:
+        factors_name += "_compile"
+    analyzer.fit_all_factors(
+        factors_name=factors_name,
+        dataset=train_dataset,
+        per_device_batch_size=args.per_device_batch_size,
+        factor_args=factor_args,
+        initial_per_device_batch_size_attempt=64,
+        overwrite_output_dir=False,
+    )
+
+    # # Compute pairwise scores.
+    # score_args = ScoreArguments()
+    # scores_name = factor_args.strategy
+    # if args.use_half_precision:
+    #     score_args = all_low_precision_score_arguments(dtype=torch.bfloat16)
+    #     scores_name += "_half"
+    # if args.use_compile:
+    #     scores_name += "_compile"
+    # if args.compute_per_token_scores:
+    #     score_args.compute_per_token_scores = True
+    #     scores_name += "_per_token"
+    # rank = args.query_gradient_rank if args.query_gradient_rank != -1 else None
+    # if rank is not None:
+    #     score_args.query_gradient_low_rank = rank
+    #     score_args.query_gradient_accumulation_steps = 10
+    #     scores_name += f"_qlr{rank}"
+    # analyzer.compute_pairwise_scores(
+    #     scores_name=scores_name,
+    #     score_args=score_args,
+    #     factors_name=factors_name,
+    #     query_dataset=eval_dataset,
+    #     train_dataset=train_dataset,
+    #     per_device_query_batch_size=args.query_batch_size,
+    #     per_device_train_batch_size=args.train_batch_size,
+    #     overwrite_output_dir=False,
+    # )
+
+    # scores = analyzer.load_pairwise_scores(scores_name)["all_modules"]  # type: ignore
+    # logging.info(f"Scores shape: {scores.shape}")
+
+
+if __name__ == "__main__":
+    main()
---
+++ quelle/approx_unrolling/language_task.py
@@ -0,0 +1,103 @@
+from typing import Dict, List, Optional
+
+import torch
+import torch.nn.functional as F
+from kronfluence.task import Task
+from torch import nn
+
+BATCH_TYPE = Dict[str, torch.Tensor]
+
+
+class LanguageModelingTask(Task):
+    def __init__(
+        self,
+        module_keys: List[str] = [],
+        track_attention: bool = True,
+        track_mlp: bool = True,
+        track_custom: List[str] = [],
+    ):
+        """
+        Initialize the LanguageModelingTask with customized influence tracking.
+
+        Args:
+            num_layers: Number of transformer layers to track
+            track_attention: Whether to track attention modules
+            track_mlp: Whether to track MLP modules
+            custom_modules: Additional custom modules to track
+            **kwargs: Other parameters for the parent class
+        """
+        super().__init__()
+        self.track_attention = track_attention
+        self.track_mlp = track_mlp
+        self.track_custom = track_custom
+        self.module_keys = module_keys
+
+    def compute_train_loss(
+        self,
+        batch: BATCH_TYPE,
+        model: nn.Module,
+        sample: bool = False,
+    ) -> torch.Tensor:
+        logits = model(
+            input_ids=batch["input_ids"],
+            attention_mask=batch["attention_mask"],
+        ).logits
+        logits = logits[..., :-1, :].contiguous()
+        logits = logits.view(-1, logits.size(-1))
+
+        if not sample:
+            labels = batch["labels"]
+            labels = labels[..., 1:].contiguous()
+            summed_loss = F.cross_entropy(logits, labels.view(-1), reduction="sum")
+        else:
+            with torch.no_grad():
+                probs = torch.nn.functional.softmax(logits.detach(), dim=-1)
+                sampled_labels = torch.multinomial(
+                    probs,
+                    num_samples=1,
+                ).flatten()
+            summed_loss = F.cross_entropy(logits, sampled_labels, reduction="sum")
+        return summed_loss
+
+    def compute_measurement(
+        self,
+        batch: BATCH_TYPE,
+        model: nn.Module,
+    ) -> torch.Tensor:
+        # We could also compute the log-likelihood or averaged margin.
+        return self.compute_train_loss(batch, model)
+
+    # def get_influence_tracked_modules(self) -> List[str]:
+    #     total_modules = []
+
+    #     for i in range(12):
+    #         total_modules.append(f"transformer.h.{i}.attn.c_attn")
+    #         total_modules.append(f"transformer.h.{i}.attn.c_proj")
+
+    #     for i in range(12):
+    #         total_modules.append(f"transformer.h.{i}.mlp.c_fc")
+    #         total_modules.append(f"transformer.h.{i}.mlp.c_proj")
+    #     print(total_modules)
+    #     return total_modules
+
+    def get_influence_tracked_modules(self) -> Optional[List[str]]:
+        """Will track everything if no modules are specified."""
+        total_modules = []
+
+        for m in self.module_keys:
+            if any(x in m.lower() for x in ["dropout", "layernorm", "act"]):
+                continue
+            if (
+                "attention." in m.lower() or "attn." in m.lower()
+            ) and self.track_attention:
+                total_modules.append(m)
+            if "mlp." in m.lower() and self.track_mlp:
+                total_modules.append(m)
+
+        if self.track_custom:
+            total_modules += self.track_custom
+
+        return total_modules if len(total_modules) > 0 else None
+
+    def get_attention_mask(self, batch: BATCH_TYPE) -> torch.Tensor:
+        return batch["attention_mask"]
