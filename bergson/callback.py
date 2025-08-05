Fix distributed on-the-fly gradient collection indexing

bergson/callback.py
@@ -1,10 +1,12 @@
 import os
+from functools import wraps
 
 import numpy as np
 import torch
 import torch.distributed as dist
 from numpy.typing import DTypeLike
 from torch import Tensor
+from transformers.trainer import Trainer
 from transformers.trainer_callback import TrainerCallback, TrainerControl, TrainerState
 from transformers.training_args import TrainingArguments
 
@@ -53,6 +55,10 @@ class GradientCollectorCallback(TrainerCallback):
         self.mean_gradients = mean_gradients
         self.normalize_eval = normalize_eval
 
+        self.eval_grad_buffers: dict[str, np.memmap] = {}
+        self.eval_step_idxs: dict[str, int] = {}
+        self.eval_indices: dict[str, list[int]] = {}
+
     def write_grads(
         self,
         grad_buffer: np.memmap,
@@ -63,6 +69,41 @@ class GradientCollectorCallback(TrainerCallback):
             torch.cuda.synchronize()
             grad_buffer[layer_name][indices] = mod_grads[layer_name].numpy()
         self.mod_grads.clear()
+
+    def on_train_begin(
+        self,
+        args: TrainingArguments,
+        state: TrainerState,
+        control: TrainerControl,
+        **kwargs,
+    ):
+        assert hasattr(args, "__gradient_collection_enabled__"), (
+            "Gradient collection is not enabled. Please enable it by "
+            "calling bergson.prepare_gradient_collection on the trainer."
+        )
+
+    def on_train_end(
+        self,
+        args: TrainingArguments,
+        state: TrainerState,
+        control: TrainerControl,
+        **kwargs,
+    ):
+        if not self.mean_gradients:
+            return
+
+        num_epochs = args.num_train_epochs
+
+        # Divide all the summed gradient buffers by the number of epochs
+        self.train_grad_buffer /= num_epochs
+        self.train_grad_buffer.flush()
+
+        if not self.eval_grad_buffers:
+            return
+
+        for grad_buffer in self.eval_grad_buffers.values():
+            grad_buffer /= num_epochs
+            grad_buffer.flush()
 
     def on_epoch_begin(
         self,
@@ -78,11 +119,21 @@ class GradientCollectorCallback(TrainerCallback):
 
         epoch_suffix = "" if self.mean_gradients else f"/epoch_{epoch}"
 
+        if dist.is_initialized():
+            num_examples = torch.tensor(
+                [len(kwargs["train_dataloader"].dataset)],
+                device="cuda",
+                dtype=torch.int32,
+            )
+            dist.all_reduce(num_examples, op=dist.ReduceOp.SUM)
+            self.num_examples = int(num_examples.item())
+        else:
+            self.num_examples = len(kwargs["train_dataloader"].dataset)
+
         # Set up the gradient buffers for the training dataset
         self.train_indices = [
             i.item() for batch in kwargs["train_dataloader"] for i in batch["_idx"]
         ]
-        self.num_examples = len(self.train_indices)
         self.train_grad_buffer = create_index(
             os.path.join(self.path, "train" + epoch_suffix),
             num_grads=self.num_examples,
@@ -98,10 +149,6 @@ class GradientCollectorCallback(TrainerCallback):
             eval_datasets = kwargs["eval_dataloader"]
         else:
             eval_datasets = {"eval": kwargs["eval_dataloader"]}
-
-        self.eval_grad_buffers = {}
-        self.eval_step_idxs = {}
-        self.eval_indices = {}
 
         for dataset_name, dataloader in eval_datasets.items():
             self.eval_grad_buffers[dataset_name] = create_index(
@@ -196,3 +243,40 @@ class GradientCollectorCallback(TrainerCallback):
             self.mod_grads,
             indices,
         )
+
+
+def prepare_for_gradient_collection(trainer: Trainer):
+    """Mutate the trainer object and its datasets in-place to expose the dataset
+    indices to the gradient collector callback."""
+    # Add indices to the training dataset
+    trainer.train_dataset = trainer.train_dataset.map(
+        lambda ex, idx: {"_idx": idx}, with_indices=True
+    )
+
+    # Add indices to the evaluation dataset/s if they exist
+    if trainer.eval_dataset is not None:
+        if isinstance(trainer.eval_dataset, dict):
+            for eval_name, dataset in trainer.eval_dataset.items():
+                trainer.eval_dataset[eval_name] = dataset.map(
+                    lambda ex, idx: {"_idx": idx}, with_indices=True
+                )
+        else:
+            trainer.eval_dataset = trainer.eval_dataset.map(
+                lambda ex, idx: {"_idx": idx}, with_indices=True
+            )
+
+    # Mutate the trainer to retain the indices
+    def retain_idx(collator):
+        @wraps(collator)
+        def wrapper(features):
+            batch = collator(features)
+            batch.setdefault("_idx", torch.tensor([f["_idx"] for f in features]))
+            return batch
+
+        return wrapper
+
+    trainer.data_collator = retain_idx(trainer.data_collator)
+    trainer.args.remove_unused_columns = False
+    trainer.args.__gradient_collection_enabled__ = True  # type: ignore
+
+    return trainer

examples/trainer_gradient_collection.py
@@ -3,7 +3,6 @@ import os
 import socket
 from dataclasses import dataclass
 from datetime import timedelta
-from functools import wraps
 from typing import Literal
 
 import torch
@@ -17,11 +16,13 @@ from transformers import AutoModelForCausalLM, AutoTokenizer
 from transformers.modeling_utils import PreTrainedModel
 from trl import SFTConfig, SFTTrainer, setup_chat_format
 
-from bergson import GradientCollector, GradientProcessor
-from bergson.callback import GradientCollectorCallback
+from bergson import (
+    GradientCollector,
+    GradientCollectorCallback,
+    GradientProcessor,
+    prepare_for_gradient_collection,
+)
 from bergson.collection import fit_normalizers
-
-# from trl.trainer.sft_trainer import DataCollatorForLanguageModeling
 from bergson.data import (
     DataConfig,
     IndexConfig,
@@ -197,64 +198,6 @@ def mean_query(datasets: list[str], batch_size: int):
     return torch.stack(queries).mean(0)
 
 
-def add_gradient_collector_callback(
-    trainer, gradient_collector, processor, mod_grads, preconditioners, run_name
-):
-    """Mutate the trainer object and its datasets in-place to expose the dataset
-    indices to the gradient collector callback."""
-    # Add indices to the training dataset
-    if trainer.train_dataset is not None:
-        trainer.train_dataset = trainer.train_dataset.map(
-            lambda ex, idx: {"_idx": idx}, with_indices=True
-        )
-
-    # Add indices to the evaluation dataset/s if they exist
-    eval_datasets = {}
-    if trainer.eval_dataset is not None:
-        if isinstance(trainer.eval_dataset, dict):
-            for eval_name, dataset in trainer.eval_dataset.items():
-                trainer.eval_dataset[eval_name] = dataset.map(
-                    lambda ex, idx: {"_idx": idx}, with_indices=True
-                )
-                eval_datasets[eval_name] = len(trainer.eval_dataset[eval_name])
-        else:
-            trainer.eval_dataset = trainer.eval_dataset.map(
-                lambda ex, idx: {"_idx": idx}, with_indices=True
-            )
-            eval_datasets["eval"] = len(trainer.eval_dataset)
-
-    # Mutate the trainer to retain the indices
-    def retain_idx(collator):
-        @wraps(collator)
-        def wrapper(features):
-            batch = collator(features)
-            batch.setdefault("_idx", torch.tensor([f["_idx"] for f in features]))
-            return batch
-
-        return wrapper
-
-    trainer.data_collator = retain_idx(trainer.data_collator)
-    trainer.args.remove_unused_columns = False
-
-    # Create the callback
-    grad_sizes = {name: math.prod(s) for name, s in gradient_collector.shapes().items()}
-
-    gradient_collector_callback = GradientCollectorCallback(
-        collector=gradient_collector,
-        processor=processor,
-        mod_grads=mod_grads,
-        preconditioners=preconditioners,
-        grad_sizes=grad_sizes,
-        path=f"examples/runs/gradients/{run_name}",
-        mean_gradients=True,
-    )
-
-    # Set the callback
-    trainer.add_callback(gradient_collector_callback)
-
-    return trainer
-
-
 def worker(
     rank: int,
     world_size: int,
@@ -299,6 +242,19 @@ def worker(
         target_modules=target_modules,
     )
 
+    # Create the callback
+    grad_sizes = {name: math.prod(s) for name, s in gradient_collector.shapes().items()}
+
+    gradient_collector_callback = GradientCollectorCallback(
+        collector=gradient_collector,
+        processor=processor,
+        mod_grads=mod_grads,
+        preconditioners=preconditioners,
+        grad_sizes=grad_sizes,
+        path=f"examples/runs/gradients/{run_name}",
+        mean_gradients=True,
+    )
+
     trainer = SFTTrainer(
         model=model,
         train_dataset=train,  # type: ignore
@@ -321,9 +277,12 @@ def worker(
         ),
     )
 
-    trainer = add_gradient_collector_callback(
-        trainer, gradient_collector, processor, mod_grads, preconditioners, run_name
-    )
+    # Set the callback
+    trainer = prepare_for_gradient_collection(trainer)
+    trainer.add_callback(gradient_collector_callback)
+
+    train.set_format("torch")
+    eval.set_format("torch")
 
     with gradient_collector:
         trainer.train()